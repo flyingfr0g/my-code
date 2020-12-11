297dfeb7b06c2ad1cdebc11ea32b1ed7919e6f90
Alexander <alexander.b.scull@gmail.com>
2020-12-11 17:06:49 +0000

    challenge

---
+++ challenges/funtion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#first we prompt the user for an input
+
+name = input("greetings traveller what is your name? ")
+
+#next we create a funtion to interpret the name and print hello next to it
+
+def hello():
+    response = (f"Hello {name}")
+    print(response)
+
+hello()  #this calls our newly created funtion.
+
+#next we will ask the user for a number
+
+number = input("what is your favorite number?  ")
+
+#next create a function that takes that number and multiplies it by 11
+
+def multiply():
+    calculation = number * 11
+    print(f"ahh well here it is multiplied by 11: {calculation}")
+    return calculation
+
+
+multiply()
